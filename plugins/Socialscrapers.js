05f08c1d444e3f6479ed9a286a1b5c954a1b4091
Thilina30599 <79817669+Thilina30599@users.noreply.github.com>
2021-09-19 18:18:08 +0530

    Update Socialscrapers.js

--- plugins/Socialscrapers.js
+++ plugins/Socialscrapers.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const Config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('amazone');
-const capt = "🎭 *NeotroX* 🕊"
+const capt = "🎭 *Public-Bot* 🕊"
 
 if (Config.WORKTYPE == 'public') {
 	
